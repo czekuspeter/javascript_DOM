7188ac53a43c20a7289531cc93edec31fdfa291c
Czékus Péter <144217254+czekuspeter@users.noreply.github.com>
2023-10-13 11:53:23 +0200

    Add files via upload

---
+++ kod.js
@@ -0,0 +1,25 @@
+//1.feladat:
+let sz = document.getElementById("elem1");
+sz.style.backgroundColor = "yellow";
+ sz.innerHTML = "Első elem";
+
+ //2.feladat:
+
+ let sz2 = document.getElementsByClassName("item2")[0];
+ sz2.innerHTML = "Második elem";
+sz2.style.color = "red"; 
+
+//3.feladat:
+
+document.body.style.backgroundColor ="purple";
+
+//4.feladat:
+
+let sz3 = document.getElementById("elem3");
+sz3.innerHTML = "Harmadik elem";
+
+
+//5.feladat:
+
+let sz4 = document.getElementById("elem4");
+sz4.innerHTML = `<img src="https://miro.medium.com/v2/resize:fit:2000/0*mfUNFSHyrXnH7ZKf"  width="300" height="200">`;
